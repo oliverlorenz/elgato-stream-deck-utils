1a7f1b42ffc9158030abac7920c4dcd55f229afc
Oliver Lorenz <mail@oliverlorenz.com>
2020-11-09 23:10:08 +0100

    fix: libs

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,12 @@
 module.exports = {
-  // coverageThreshold: {
-  //   global: {
-  //     branches: 100,
-  //     functions: 100,
-  //     lines: 100,
-  //     statements: 0,
-  //   },
-  // },
+  coverageThreshold: {
+    //   global: {
+    //     branches: 100,
+    //     functions: 100,
+    //     lines: 100,
+    //     statements: 0,
+    //   },
+  },
   coverageDirectory: 'coverage',
 
   moduleFileExtensions: [
